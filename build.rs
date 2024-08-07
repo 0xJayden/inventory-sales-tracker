5757882799820e6480692b558f62a987c7cd18fe
0xJayden <gannixcrypto@protonmail.com>
2024-08-07 14:18:42 -0700

    updated build script

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
 extern crate winres;
+use crate std::env;
 
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("/Users/jaydendavila/phgnew/assets/icon.ico");
+        let contents = env::current_exe().unwrap().parent().unwrap().parent().unwrap().parent().unwrap().to_str().unwrap();
+        res.set_icon(format!("{}/assets/icon.ico", contents));
         res.compile().unwrap();
     }
 }
